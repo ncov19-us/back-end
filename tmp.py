c26d461860bb9e6dc85bc98f88c6b20f5a289261
leehanchung <kein520@yahoo.com>
2020-03-31 19:22:35 -0700

    han: mongo ingestion wip

--- tmp.py
+++ tmp.py
@@ -1,4 +1,5 @@
 from decouple import config
+import pandas as pd
 from mongoengine import *
 
 # from api.config import MONGODB_CONNECTION_URI
@@ -50,44 +51,79 @@ class County(Document):
 
 
 def main():
-    connect(host=config("MONGODB_CONNECTION_URI"))
-
-    county = County(state="Michigan",
-                    stateAbbr="MI",
-                    county="Deer Field",
-                    fips=12345,
-                    lat=12.34567,
-                    lon=89.01234,
-                    stats=Stats(
-                        last_updated="2020-03-31",
-                        confirmed=12345,
-                        deaths=67890,
-                        )
-                    )
-    county.save()
-
-    country = Country(country="South Korea",
-                alpha2Code="SK",
-                lat=12.34567,
-                lon=89.01234,
-                stats=Stats(
-                    last_updated="2020-03-31",
-                    confirmed=12345,
-                    deaths=67890,
-                    )
-                )
-    country.save()
-
-
-    for county in County.objects:
-        print(county.state)
     
-    for country in Country.objects:
-        print(country.state)
+    confirmed = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
+    deaths = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv"
 
-    disconnect()
-    # tm = TestMongo(db_name="covid-staging", collection_name="county")
+    confirmed = pd.read_csv(confirmed)
+    deaths = pd.read_csv(deaths)
+    
+    print(f"Total Number of Countries confirmed {confirmed['Country/Region'].unique().shape}")
+    print(f"Total Number of Countries deaths {deaths['Country/Region'].unique().shape}")
+
+    confirmed = confirmed.groupby('Country/Region').sum()
+    deaths = deaths.groupby('Country/Region').sum()
+    confirmed = confirmed.sort_index().reset_index()
+    deaths = deaths.sort_index().reset_index()
+
+    # sanity check
+    assert confirmed.shape == deaths.shape
+    assert confirmed['Country/Region'][37] == deaths['Country/Region'][37]
+    assert confirmed['Country/Region'][115] == deaths['Country/Region'][115]
+    assert confirmed['Country/Region'][175] == deaths['Country/Region'][175]
+    
+
+    # print(confirmed.sample(5))
+
+    connect(host=config("MONGODB_CONNECTION_URI"))
+    
+    for i in range(5):
+        print(confirmed.iloc[i])
+        country = Country(confirmed[i])
+        country = Country(
+                    country=confirmed.iloc['Contry/Region']
+                    # alpha2Code=
+        )
+    
+    # country = Country(country="South Korea",
+    #             alpha2Code="SK",
+    #             lat=12.34567,
+    #             lon=89.01234,
+    #             stats=Stats(
+    #                 last_updated="2020-03-31",
+    #                 confirmed=12345,
+    #                 deaths=67890,
+    #                 )
+    #             )
+    # country.save()
+
+    # for country in Country.objects:
+    #     print(country.state)
+
+    # disconnect()
+
+
+    # for county in County.objects:
+    #     print(county.state)
+    
+    # county = County(state="Michigan",
+    #                 stateAbbr="MI",
+    #                 county="Deer Field",
+    #                 fips=12345,
+    #                 lat=12.34567,
+    #                 lon=89.01234,
+    #                 stats=Stats(
+    #                     last_updated="2020-03-31",
+    #                     confirmed=12345,
+    #                     deaths=67890,
+    #                     )
+    #                 )
+    # county.save()
+
+
+    # # tm = TestMongo(db_name="covid-staging", collection_name="county")
     # print(tm.collection.find())
+
 
 
 if __name__ == "__main__":
