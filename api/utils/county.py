f78093260c049f49570abdac4f5c5e4ca5fb74f1
leehanchung <kein520@yahoo.com>
2020-04-04 14:32:45 -0700

    han: county.py: fixed new death data using time series. Pipfile: added pytest to dev

--- api/utils/county.py
+++ api/utils/county.py
@@ -18,13 +18,28 @@ def read_county_data() -> pd.DataFrame:
 
 
 def read_county_stats(state: str, county: str) -> pd.DataFrame:
-    df = pd.read_csv(Config.COUNTY_URL)
-    df.columns = map(str.lower, df.columns)
-    df.columns = df.columns.str.replace(" ", "_")
+    
     try:
+        df = pd.read_csv(Config.COUNTY_URL)
+        deaths = pd.read_csv(Config.STATE_DEATH)
+    except:
+        raise ValueError(
+            f"Data reading error State: {state}, and County: {county}."
+        )
+    
+    try:
+        df.columns = map(str.lower, df.columns)
+        df.columns = df.columns.str.replace(" ", "_")
+
+        # used data source 2 for new death number
+        deaths = deaths[deaths['Province_State'] == reverse_states_map[state]]
+        deaths = deaths[deaths['Admin2'] == county]
+        deaths = deaths.iloc[:, 12:].diff(axis=1).iloc[:, -1].values[0]    
+        
         df = df[df["state_name"] == reverse_states_map[state]]
         # df = df.query(f"county_name == '{county}'")
         df = df[df["county_name"] == county]
+        df.new_death.iloc[0] = deaths
         df = pd.DataFrame.to_dict(df, orient="records")
         if len(df) == 0:
             raise
